dfb844fbf54a718d6e00138521dd2f6a57e28108
Kupina <mkupina@ur.rochester.edu>
2019-07-15 11:59:49 -0400

    First submission 3_week

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pythonds9",
+    version="0.0.0",
+    aythor="Marina Kupina",
+    author_email='mkupina@ur.rochester.edu",
+    description="Stuff for Python Algs and Data Structure",
+    url="https://github.com/maritum/pythonds9.git",
+    packages=setuptools.find_packages()
+    )
